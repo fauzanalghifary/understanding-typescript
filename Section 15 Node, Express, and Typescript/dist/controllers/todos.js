06d196e17b45f4d57a1a90414d679a7949b0c98a
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-11-28 21:58:06 +0700

    finish course

---
+++ Section 15 Node, Express, and Typescript/dist/controllers/todos.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
+const TODOS = [];
+const createTodo = (req, res, next) => {
+    const text = req.body.text;
+    const newTodo = { id: new Date().toISOString(), text };
+    TODOS.push(newTodo);
+    res.status(201).json({ message: "Created the todo.", createdTodo: newTodo });
+};
+exports.createTodo = createTodo;
+const getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.getTodos = getTodos;
+const updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("Could not find todo!");
+    }
+    TODOS[todoIndex] = { id: TODOS[todoIndex].id, text: updatedText };
+    res.json({ message: "Updated!", updatedTodo: TODOS[todoIndex] });
+};
+exports.updateTodo = updateTodo;
+const deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("Could not find todo!");
+    }
+    TODOS.splice(todoIndex, 1);
+    res.json({ message: "Todo deleted!" });
+};
+exports.deleteTodo = deleteTodo;
