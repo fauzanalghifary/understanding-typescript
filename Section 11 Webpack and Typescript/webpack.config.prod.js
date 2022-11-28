cc7351022f55f75e315c65de9b0e256fdd9dcf39
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-11-28 13:03:50 +0700

    finishsection 11

---
+++ Section 11 Webpack and Typescript/webpack.config.prod.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
